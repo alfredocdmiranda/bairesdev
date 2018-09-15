1c1b7918e2c3f0efcd0d842035646ae62ef073d5
Alfredo Miranda <alfredocdmiranda@gmail.com>
2018-09-15 11:02:00 -0300

    Add initial models

--- project/core/models.py
+++ project/core/models.py
@@ -2,4 +2,21 @@ import os
 
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
+
+RATINGS = [('1',1),('2',2),('3',3),('4',4),('5',5)]
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=64)
+    summary = models.TextField(max_length=10000)
+    ratings = models.IntegerField(choices=RATINGS)
+    ip_addr = models.CharField(max_length=39)
+    submission_date = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.SET_NULL, null=True)
--- project/core/serializers.py
+++ project/core/serializers.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
 
 from .models import *
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    company = CompanySerializer(read_only=True)
+    company_id = serializers.PrimaryKeyRelatedField(source='company', queryset=Company.objects.all(), )
+
+    class Meta:
+        model = Review
